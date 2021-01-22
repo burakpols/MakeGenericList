94a4843b3b13ff4505e35db342cc08465f070135
burakpols <burakpolsq@gmail.com>
2021-01-22 14:40:07 +0300

    Proje dosyası ekle.

---
+++ MakeGenericList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MakeGenericList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> numbers = new MyList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                numbers.Add(i);
+                Console.WriteLine("Added Numbers: "+i);
+
+            }
+
+            Console.WriteLine("\nHow many items in numbers array: "+numbers.Count);
+            
+
+        }
+    }
+}
